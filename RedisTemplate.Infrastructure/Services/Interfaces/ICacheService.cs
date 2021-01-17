86b510f8f901e0b146e4fcec1eedcb123c9dfd84
Vural Vardarelli <vuralvardarelli@hotmail.com>
2021-01-17 18:16:51 +0300

    redis crud ops

---
+++ RedisTemplate.Infrastructure/Services/Interfaces/ICacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisTemplate.Infrastructure.Services.Interfaces
+{
+    public interface ICacheService
+    {
+        Task<T> Get<T>(string key);
+        Task Add(string key, object data);
+        Task Remove(string key);
+        Task<bool> Any(string key);
+    }
+}
---
+++ RedisTemplate.Infrastructure/Services/RedisCacheService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using RedisTemplate.Infrastructure.Data;
+using RedisTemplate.Infrastructure.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisTemplate.Infrastructure.Services
+{
+    public class RedisCacheService : ICacheService
+    {
+        private CacheContext _context;
+
+        public RedisCacheService(CacheContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(string key, object data)
+        {
+            string jsonData = JsonConvert.SerializeObject(data);
+            await _context.Redis.StringSetAsync(key, jsonData);
+        }
+
+        public async Task<bool> Any(string key)
+        {
+            return await _context.Redis.KeyExistsAsync(key);
+        }
+
+        public async Task<T> Get<T>(string key)
+        {
+            if(await Any(key))
+            {
+                string jsonData = await _context.Redis.StringGetAsync(key);
+                return JsonConvert.DeserializeObject<T>(jsonData);
+            }
+
+            return default;
+        }
+
+        public async Task Remove(string key)
+        {
+            await _context.Redis.KeyDeleteAsync(key);
+        }
+    }
+}
